4e0838bf94260766b91f941c9c8cae41493071c9
Zivision <zcastro703@gmail.com>
2025-09-29 08:58:21 -0500

    Added api test input

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from datetime import datetime
 
 # Setup Flask
 app = Flask(__name__)
@@ -7,6 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+# Test end point
+@app.route("/api/test")
+def test() -> dict:
+    return { 
+        "status": "running",
+        "time": datetime.now()
+    }
 
 # Wrapper function with params
 def run_app(host="127.0.0.1", port=8080, debug=True) -> None:
